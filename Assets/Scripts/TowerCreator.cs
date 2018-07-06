9517cb902896ea620cdf5cd2a4866db2532ba2cf
nice_shot <shefferu@gmail.com>
2018-07-06 10:33:05 +0300

    Created method for tower building and started working on ray casting

--- Assets/Scripts/TowerCreator.cs
+++ Assets/Scripts/TowerCreator.cs
@@ -16,7 +16,13 @@ public class TowerCreator : MonoBehaviour, ITrackableEventHandler {
     }
 
     void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit)) {
 
+            }
+        }
     }
 
     public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus,
@@ -26,8 +32,13 @@ public class TowerCreator : MonoBehaviour, ITrackableEventHandler {
             newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED) {
             Debug.Log("Found image!");
             // Create tower in the middle of map
-            Instantiate(TowerPrefab, this.transform, false);
+            //Instantiate(TowerPrefab, this.transform, false);
+            BuildTower(transform.position, transform.rotation);
         }
+    }
+
+    private void BuildTower(Vector3 position, Quaternion rotation) {
+        Instantiate(TowerPrefab, position, rotation, this.transform);
     }
 
 }
