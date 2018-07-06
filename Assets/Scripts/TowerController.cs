4f9b19a8e38c1f4abafc0e56df60a158253e30bd
nice_shot <shefferu@gmail.com>
2018-07-06 13:58:47 +0300

    Added sound for building apearance. Maybe a bit cartoony but its cute

---
+++ Assets/Scripts/TowerController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerController : MonoBehaviour {
+
+    private AudioSource audioSource;
+
+	void Awake () {
+        audioSource = GetComponent<AudioSource>();
+        // Set random pitch to add variance when building rises
+        audioSource.pitch = Random.Range(0.7f, 1.6f);
+	}
+}
